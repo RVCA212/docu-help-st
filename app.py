9fbddf0d01c903cd71de1b21a5538ba93f4e4bc7
RVCA212 <112829052+RVCA212@users.noreply.github.com>
2024-02-02 19:46:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def main():
         st.header("Configuration")
         model_option = st.radio(
             "Choose your model:",
-            ("gpt-4-1106-preview", "gpt-3.5-turbo-1106")
+            ("gpt-4-0125-preview", "gpt-3.5-turbo-1106")
         )
         pinecone_index_name = st.text_input("Enter Pinecone Index Name")
 
