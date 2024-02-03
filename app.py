daf4f01f05de25d2c575f2552c79e5ab7171df82
RVCA212 <112829052+RVCA212@users.noreply.github.com>
2024-02-02 20:04:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough
 from langchain_openai import ChatOpenAI
-import openai  # Ensure you have imported OpenAI's SDK
+from openai import OpenAI  # Ensure you have imported OpenAI's SDK
 
 # Streamlit App
 def main():
