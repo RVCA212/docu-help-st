7afe544ac46c8a9320c43e1e9ff9842cb242c46f
RVCA212 <112829052+RVCA212@users.noreply.github.com>
2024-02-20 12:21:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -107,8 +107,8 @@ container = st.container()
 
 with container:
     with st.form(key='my_form', clear_on_submit=True):
-    user_input = st.text_area("You:", key='input', height=100)
-    submit_button = st.form_submit_button(label='Send')
+        user_input = st.text_area("You:", key='input', height=100)
+        submit_button = st.form_submit_button(label='Send')
 
     if submit_button and user_input:
         st.session_state['past'].append(user_input)
